1a49cc493231097e0ff98eff4a720679a46c4bbc
extazystas <2153311+Extazystas@users.noreply.github.com>
2024-01-20 18:20:19 +0200

    Add Day 2 part 2 solution

--- execute.rb
+++ execute.rb
@@ -19,4 +19,4 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
   Dir["#{BASE_PATH}/#{dir}/*.rb"].each { |file| require_relative file }
 end
 
-p Day02.call
+p Day02p2.call
--- lib/day02/part2.rb
+++ lib/day02/part2.rb
@@ -1,4 +1,6 @@
 class Day02p2
+  COLORS = %w(red green blue)
+
   class << self
     def call
       file = File.open('lib/day02/input1.txt', 'r')
@@ -6,6 +8,22 @@ class Day02p2
     end
 
     def process(input)
+      matched_ids_sum = 0
+      input.each_line do |line|
+        match_data = line.match(/Game (\d+): (.+)/)
+        game_id    = match_data[1]
+        games_info = match_data[2].split('; ')
+        memoized_games = Hash[COLORS.product([0])]
+
+        games_info.each do |game|
+          game.split(', ').each do |cube|
+            value, color = cube.split
+            memoized_games[color] = value.to_i if memoized_games[color] && memoized_games[color] < value.to_i
+          end
+        end
+        matched_ids_sum += memoized_games.values.inject(:*)
+      end
+      matched_ids_sum
     end
   end
 end
@@ -13,8 +31,13 @@ end
 class Test < Minitest::Test
   def test_day2_part2
     assert_data = <<~DATA
+      Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+      Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+      Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+      Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+      Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
     DATA
 
-    # assert_equal 281, Day02p2.process(assert_data)
+    assert_equal 2286, Day02p2.process(assert_data)
   end
 end
